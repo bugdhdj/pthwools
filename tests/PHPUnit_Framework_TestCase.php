7ce42be3dda5638faa8b87e4ee3c379d3e974961
lixworth <lixworth@gmail.com>
2021-07-27 18:21:37 +0800

    update composer config

---
+++ tests/PHPUnit_Framework_TestCase.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * PHPUnit_Framework_TestCase.php
+ *
+ * @project pthwools
+ * @author lixworth <lixworth@outlook.com>
+ * @copyright pthwools
+ * @create 2021/7/27 18:21
+ */
+
+declare(strict_types=1);
